b61c896b05fd5941012c163250a2b67fd3b820ff
fmorgante <fmorgante@uchicago.edu>
2020-07-29 18:46:59 -0500

    Add wb argument

--- dsc/modules/simulate_data_mod.R
+++ dsc/modules/simulate_data_mod.R
@@ -1,3 +1,3 @@
-out <- simulate_data(n, p, p_causal, r, r_causal,
+out <- simulate_data(n, p, p_causal, r, r_causal, wb,
                      pve, B_cor, B_scale, w, X_cor, 
                      X_scale, V_cor, prop_testset)
