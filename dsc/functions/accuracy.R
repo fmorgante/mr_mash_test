ec02ab06ce38bd579328be086369acb9015d17e3
fmorgante <fmorgante@uchicago.edu>
2020-09-30 12:18:38 -0500

    Handle cases where var(yhat)=0

--- dsc/functions/accuracy.R
+++ dsc/functions/accuracy.R
@@ -4,10 +4,17 @@ compute_accuracy <- function(Y, Yhat) {
   rmse <- rep(as.numeric(NA), ncol(Y))
   
   for(i in 1:ncol(Y)){
-    fit  <- lm(Y[, i] ~ Yhat[, i])
-    bias[i] <- coef(fit)[2] 
-    r2[i] <- summary(fit)$r.squared
-    rmse[i] <- sqrt(mean((Y[, i] - Yhat[, i])^2))
+    if(var(Yhat[, i])>0){
+      fit  <- lm(Y[, i] ~ Yhat[, i])
+      bias[i] <- coef(fit)[2] 
+      r2[i] <- summary(fit)$r.squared
+      rmse[i] <- sqrt(mean((Y[, i] - Yhat[, i])^2))
+    } else {
+      bias[i] <- NA
+      r2[i] <- NA
+      rmse[i] <- sqrt(mean((Y[, i] - Yhat[, i])^2))
+    }
+
   }
   
   return(list(bias=bias, r2=r2, rmse=rmse, scaled_rmse=rmse/matrixStats::colSds(Y)))
