71bd493e1048889516840b311cfb5845b526e9fb
fmorgante <fmorgante@uchicago.edu>
2020-10-02 10:49:28 -0500

    Set number of test set rather than proportion

--- code/sample_testset_indeces.R
+++ code/sample_testset_indeces.R
@@ -9,13 +9,13 @@ library(optparse)
 
 ###Parse arguments
 parser <- OptionParser()
-parser <- add_option(parser, c("--prop_testset"), type="numeric")
+parser <- add_option(parser, c("--n_testset"), type="integer")
 parser <- add_option(parser, c("--n"), type="integer")
 parser <- add_option(parser, c("--output"), type="character")
 parser <- add_option(parser, c("--seed"), type="integer")
 outparse <- parse_args(parser)
 
-prop_testset <- outparse$prop_testset
+n_testset <- outparse$prop_testset
 n <- outparse$n
 output <- outparse$output
 ranseed <- outparse$seed
@@ -24,7 +24,7 @@ ranseed <- outparse$seed
 set.seed(ranseed)
 
 ###Sample indeces for test set individuals
-test_set <- sort(sample(x=c(1:n), size=round(n*prop_testset), replace=FALSE))
+test_set <- sort(sample(x=c(1:n), size=n_testset, replace=FALSE))
 
 ###Save index to file
 saveRDS(test_set, output)
