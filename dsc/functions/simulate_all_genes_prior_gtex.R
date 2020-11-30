7f62c4ad6079b8a016a8c671af1f2fbdec329e46
fmorgante <fmorgante@uchicago.edu>
2020-11-30 13:14:44 -0600

    Move sampling of number of effects inside the function

--- dsc/functions/simulate_all_genes_prior_gtex.R
+++ dsc/functions/simulate_all_genes_prior_gtex.R
@@ -1,5 +1,9 @@
 simulate_data_all_genes_prior_gtex <- function(X, p_causal, r, r_causal, pve, B_cor, B_scale, w, V_cor, testset_index){
   
+  if(!is.numeric(p_causal)){
+    p_causal <- sample(x=eval(parse(text=p_causal)), size=1)
+  }
+  
   dat <- simulate_mr_mash_data_from_given_X(X=X, p_causal=p_causal, r=r, r_causal=r_causal, intercepts=rep(1, r),
                                             pve=pve, B_cor=B_cor, B_scale=B_scale, w=w, V_cor=V_cor)
   
