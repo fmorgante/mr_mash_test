6fbf9b66030044680364b4518d98d74719078e4e
Fabio Morgante <fabiom@clemson.edu>
2021-04-21 14:52:49 -0500

    Add code to compute missing Y rate by tissue

---
+++ code/get_missing_Y_rate_by_tissue_gtex_v8_manifest.R
@@ -0,0 +1,40 @@
+options(stringsAsFactors = FALSE)
+
+###Load libraries
+library(foreach)
+library(doMC)
+library(optparse)
+library(dplyr)
+
+###Parse arguments
+parser <- OptionParser()
+parser <- add_option(parser, c("--n_cores"), type="integer")
+outparse <- parse_args(parser)
+
+
+###Register cores
+registerDoMC(outparse$n_cores)
+
+###Load complete manifest
+manifest <- read.table("../data/gtex-v8-manifest.txt", header=FALSE)
+
+###Obtain missing Y rate
+res <- foreach(i = 1:nrow(manifest), .combine='bind_rows') %dopar% {
+  Y <- readRDS(manifest[i, 1])$y_res
+  missing_rate <- apply(Y, 2, function(x){sum(is.na(x))})/nrow(Y)
+  missing_rate
+}
+
+res <- as.data.frame(res)
+
+###Add names to the results
+gene_names <- vector("character", nrow(manifest))
+for(j in 1:nrow(manifest)){
+  gene_names[j] <- paste(head(unlist(strsplit(tail(unlist(strsplit(manifest[j, 1], "/")), 1), ".", fixed=TRUE)), 2), collapse=".")
+}
+
+gtex_missing_rate <- cbind(Gene_Name=gene_names, res)
+
+###Write out results
+write.table(gtex_missing_rate, "../data/gtex-v8-missing-rate-by-tissue.txt", sep="\t", row.names = FALSE, col.names=FALSE, quote=FALSE)
+
