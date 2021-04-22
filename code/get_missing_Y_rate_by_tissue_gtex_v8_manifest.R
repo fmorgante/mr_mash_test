8c3901f905875785619b8ab46e0e51f4dfdd6a78
Fabio Morgante <fabiom@clemson.edu>
2021-04-22 14:13:33 -0500

    Minor improvement

--- code/get_missing_Y_rate_by_tissue_gtex_v8_manifest.R
+++ code/get_missing_Y_rate_by_tissue_gtex_v8_manifest.R
@@ -18,23 +18,20 @@ registerDoMC(outparse$n_cores)
 ###Load complete manifest
 manifest <- read.table("../data/gtex-v8-manifest.txt", header=FALSE)
 
-###Obtain missing Y rate
-res <- foreach(i = 1:nrow(manifest), .combine='bind_rows') %dopar% {
-  Y <- readRDS(manifest[i, 1])$y_res
-  missing_rate <- apply(Y, 2, function(x){sum(is.na(x))})/nrow(Y)
-  missing_rate
-}
-
-res <- as.data.frame(res)
-
-###Add names to the results
+###Obtain gene names
 gene_names <- vector("character", nrow(manifest))
 for(j in 1:nrow(manifest)){
   gene_names[j] <- paste(head(unlist(strsplit(tail(unlist(strsplit(manifest[j, 1], "/")), 1), ".", fixed=TRUE)), 2), collapse=".")
 }
+manifest <- cbind(manifest, gene_names)
 
-gtex_missing_rate <- cbind(Gene_Name=gene_names, res)
+###Obtain missing Y rate
+res <- foreach(i = 1:nrow(manifest), .combine='bind_rows') %dopar% {
+  Y <- readRDS(manifest[i, 1])$y_res
+  missing_rate <- apply(Y, 2, function(x){sum(is.na(x))})/nrow(Y)
+  data.frame(Gene_Name=manifest[i, 2], rbind(missing_rate))
+}
 
 ###Write out results
-write.table(gtex_missing_rate, "../data/gtex-v8-missing-rate-by-tissue.txt", sep="\t", row.names = FALSE, col.names=FALSE, quote=FALSE)
+write.table(gtex_missing_rate, "../data/gtex-v8-missing-rate-by-tissue.txt", sep="\t", row.names = FALSE, col.names=TRUE, quote=FALSE)
 
