79f49a3330d51549bcb94b989d40913f95acae70
Fabio Morgante <fabiom@clemson.edu>
2021-05-28 11:26:13 -0500

    Add intercept estimation

--- dsc/functions/fit_utmost.R
+++ dsc/functions/fit_utmost.R
@@ -1,11 +1,16 @@
 fit_utmost <- function(X, Y, nfolds){
+  Y <- scale(Y, scale=FALSE)
+  muy <- attr(Y, 'scaled:center')
+  mux <- colMeans(X)
+
   time1 <- proc.time()
 
   B <- detect_eqtls_glasso(X, Y, nfolds)
+  intercept <- drop(muy - mux %*% B)
 
   time2 <- proc.time()
 
   elapsed_time <- time2["elapsed"] - time1["elapsed"]
 
-  return(list(B_est=B, elapsed_time=elapsed_time))
+  return(list(B_est=B, intercept_est=intercept, elapsed_time=elapsed_time))
 }
