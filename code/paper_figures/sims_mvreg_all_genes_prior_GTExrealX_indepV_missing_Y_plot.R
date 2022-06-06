5919f12d9c5db3abbf043abd5db4253df5c79e43
Fabio Morgante <fabiom@clemson.edu>
2022-06-06 10:31:15 -0500

    Add code to make figures for paper for sims with missing Y

---
+++ code/paper_figures/sims_mvreg_all_genes_prior_GTExrealX_indepV_missing_Y_plot.R
@@ -0,0 +1,186 @@
+###Set options
+options(stringsAsFactors=FALSE)
+
+###Load libraries
+library(ggplot2)
+library(cowplot)
+library(scales)
+
+dsc_plots_1causalresp <- readRDS("../../output/sims_paper_figures_inter/mvreg_all_genes_prior_GTExrealX_indepV_B_1causalrespr10_indepReps_missing_Y_for_plotting.rds")
+dsc_plots_indep <- readRDS("../../output/sims_paper_figures_inter/mvreg_all_genes_prior_GTExrealX_indepV_indepB_r10_indepReps_missing_Y_for_plotting.rds")
+dsc_plots_shared <- readRDS("../../output/sims_paper_figures_inter/mvreg_all_genes_prior_GTExrealX_indepV_sharedB_r10_indepReps_missing_Y_for_plotting.rds")
+dsc_plots_2blocks <- readRDS("../../output/sims_paper_figures_inter/mvreg_all_genes_prior_GTExrealX_indepV_sharedB_2blocksr10_diffPVE_indepReps_missing_Y_for_plotting.rds")
+dsc_plots_3causalresp <- readRDS("../../output/sims_paper_figures_inter/mvreg_all_genes_prior_GTExrealX_indepV_sharedB_3causalrespr10_indepReps_missing_Y_for_plotting.rds")
+
+###Set some quantities used in the following plots
+colors <- c("cyan", "skyblue", "dodgerblue", "mediumblue", "limegreen", "green", "gold", "orange", "red", "firebrick", "darkmagenta", "mediumpurple")
+
+####mr.mash vs other methods
+###Error
+##Create plot
+methods_chosen <- c("mtlasso", "enet")
+metric_chosen <- "scaled_rrmse"
+
+#Filter data
+dsc_plots_methods_1causalresp_filt <- dsc_plots_1causalresp[which(dsc_plots_1causalresp$score_metric==metric_chosen & dsc_plots_1causalresp$method %in% methods_chosen), ]
+dsc_plots_methods_indep_filt <- dsc_plots_indep[which(dsc_plots_indep$score_metric==metric_chosen & dsc_plots_indep$method %in% methods_chosen), ]
+dsc_plots_methods_shared_filt <- dsc_plots_shared[which(dsc_plots_shared$score_metric==metric_chosen & dsc_plots_shared$method %in% methods_chosen), ]  
+dsc_plots_methods_2blocks_filt <- dsc_plots_2blocks[which(dsc_plots_2blocks$score_metric==metric_chosen & dsc_plots_2blocks$method %in% methods_chosen), ]
+dsc_plots_methods_3causalresp_filt <- dsc_plots_3causalresp[which(dsc_plots_3causalresp$score_metric==metric_chosen & dsc_plots_3causalresp$method %in% methods_chosen), ]
+
+#Extract y-axis limits
+# ymin <- min(min(dsc_plots_1causalresp_filt$score_value), min(dsc_plots_indep_filt$score_value),
+# 			min(dsc_plots_shared_filt$score_value), min(dsc_plots_2blocks_filt$score_value),
+# 			min(dsc_plots_3causalresp_filt$score_value))
+# 			
+# ymax <- max(max(dsc_plots_1causalresp_filt$score_value), max(dsc_plots_indep_filt$score_value),
+# 			max(dsc_plots_shared_filt$score_value), max(dsc_plots_2blocks_filt$score_value),
+# 			max(dsc_plots_3causalresp_filt$score_value))
+
+#Make plot objects for each scenario
+p_methods_1causalresp <- ggplot(dsc_plots_methods_1causalresp_filt, 
+                  aes_string(x = "response_fac", y = "score_value", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+#  ylim(ymin, ymax) +
+  scale_fill_manual(values = colors[c(7,12)], labels = c("smt-lasso", "e-net")) +
+  labs(x = "Tissue", y = expression(paste("RMSE relative to ", italic("mr.mash"))), fill="Method") +
+  geom_hline(yintercept=1, linetype="dotted", size=1, color = "red") +
+  theme_cowplot(font_size = 16) +
+  theme(plot.title = element_text(hjust = 0.5), legend.position="none")
+  
+p_methods_indep <- ggplot(dsc_plots_methods_indep_filt, 
+                  aes_string(x = "response_fac", y = "score_value", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+#  ylim(ymin, ymax) +
+  scale_fill_manual(values = colors[c(7,12)], labels = c("smt-lasso", "e-net")) +
+  labs(x = "Tissue", y = expression(paste("RMSE relative to ", italic("mr.mash"))), fill="Method") +
+  geom_hline(yintercept=1, linetype="dotted", size=1, color = "red") +
+  theme_cowplot(font_size = 16) +
+  theme(plot.title = element_text(hjust = 0.5), legend.position="none")
+
+p_methods_shared <- ggplot(dsc_plots_methods_shared_filt, 
+                  aes_string(x = "response_fac", y = "score_value", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+#  ylim(ymin, ymax) +
+  scale_fill_manual(values = colors[c(7,12)], labels = c("smt-lasso", "e-net")) +
+  labs(x = "Tissue", y = expression(paste("RMSE relative to ", italic("mr.mash"))), fill="Method") +
+  geom_hline(yintercept=1, linetype="dotted", size=1, color = "red") +
+  theme_cowplot(font_size = 16) +
+  theme(plot.title = element_text(hjust = 0.5), legend.position="none")
+  
+p_methods_2blocks_inter <- ggplot(dsc_plots_methods_2blocks_filt, 
+                  aes_string(x = "response_fac", y = "score_value", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+#  ylim(ymin, ymax) +
+  scale_fill_manual(values = colors[c(7,12)], labels = c("smt-lasso", "e-net")) +
+  labs(x = "Tissue", y = expression(paste("RMSE relative to ", italic("mr.mash"))), fill="Method") +
+  geom_hline(yintercept=1, linetype="dotted", size=1, color = "red") +
+  theme_cowplot(font_size = 16) +
+  theme(plot.title = element_text(hjust = 0.5))
+  
+p_methods_2blocks <- p_methods_2blocks_inter + theme(legend.position="none")
+
+p_methods_3causalresp <- ggplot(dsc_plots_methods_3causalresp_filt, 
+                  aes_string(x = "response_fac", y = "score_value", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+#  ylim(ymin, ymax) +
+  scale_fill_manual(values = colors[c(7,12)], labels = c("smt-lasso", "e-net")) +
+  labs(x = "Tissue", y = expression(paste("RMSE relative to ", italic("mr.mash"))), fill="Method") +
+  geom_hline(yintercept=1, linetype="dotted", size=1, color = "red") +
+  theme_cowplot(font_size = 16) +
+  theme(plot.title = element_text(hjust = 0.5), legend.position="none")
+  
+#Extract legend
+leg_methods <- get_legend(p_methods_2blocks_inter)
+
+#Make the multi panel plot 
+p_methods_all <- plot_grid(p_methods_shared, p_methods_indep, p_methods_2blocks,
+			   p_methods_1causalresp, p_methods_3causalresp, 
+			   leg_methods, labels = c('A', 'B', 'C', 'D', 'E'))
+
+ggsave("../../analysis/paper_figures/mvreg_all_genes_prior_GTExrealX_indepV_missing_Y_mrmash_vs_others_rmse.pdf", plot=p_methods_all, device="pdf", units="in", height=8, width=11)
+
+
+###Time
+methods_chosen <- c("mr_mash_em_data_enet", "mtlasso", "enet")
+metric_chosen <- "scaled_rrmse"
+
+#Filter data
+dsc_plots_1causalresp_methods_time <- dsc_plots_1causalresp[which(dsc_plots_1causalresp$response==1 & dsc_plots_1causalresp$score_metric==metric_chosen &
+                                                          dsc_plots_1causalresp$method %in% methods_chosen), ]
+ 
+dsc_plots_indep_methods_time <- dsc_plots_indep[which(dsc_plots_indep$response==1 & dsc_plots_indep$score_metric==metric_chosen &
+                                                          dsc_plots_indep$method %in% methods_chosen), ]
+
+dsc_plots_shared_methods_time <- dsc_plots_shared[which(dsc_plots_shared$response==1 & dsc_plots_shared$score_metric==metric_chosen &
+                                                          dsc_plots_shared$method %in% methods_chosen), ]
+
+dsc_plots_2blocks_methods_time <- dsc_plots_2blocks[which(dsc_plots_2blocks$response==1 & dsc_plots_2blocks$score_metric==metric_chosen &
+                                                          dsc_plots_2blocks$method %in% methods_chosen), ]
+
+dsc_plots_3causalresp_methods_time <- dsc_plots_3causalresp[which(dsc_plots_3causalresp$response==1 & dsc_plots_3causalresp$score_metric==metric_chosen &
+                                                          dsc_plots_3causalresp$method %in% methods_chosen), ]
+
+                                       
+p_methods_time_1causalresp <- ggplot(dsc_plots_1causalresp_methods_time, aes_string(x = "method_fac", y = "time", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+  scale_fill_manual(values = colors[c(9,7,12)], labels = c(expression(italic("mr.mash")), "smt-lasso", "e-net")) +
+  scale_y_continuous(trans="log2", breaks = trans_breaks("log2", function(x) 2^x),
+                     labels = trans_format("log2", math_format(2^.x))) +
+  labs(x = "", y = bquote(Elapsed ~time ~(log[2](seconds))), fill="Method") +
+  theme_cowplot(font_size = 16) +
+  theme(axis.line.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        legend.position="none")
+        
+p_methods_time_indep <- ggplot(dsc_plots_indep_methods_time, aes_string(x = "method_fac", y = "time", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+  scale_fill_manual(values = colors[c(9,7,12)], labels = c(expression(italic("mr.mash")), "smt-lasso", "e-net")) +
+  scale_y_continuous(trans="log2", breaks = trans_breaks("log2", function(x) 2^x),
+                     labels = trans_format("log2", math_format(2^.x))) +
+  labs(x = "", y = bquote(Elapsed ~time ~(log[2](seconds))), fill="Method") +
+  theme_cowplot(font_size = 16) +
+  theme(axis.line.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        legend.position="none")
+
+p_methods_time_shared <- ggplot(dsc_plots_shared_methods_time, aes_string(x = "method_fac", y = "time", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+  scale_fill_manual(values = colors[c(9,7,12)], labels = c(expression(italic("mr.mash")), "smt-lasso", "e-net")) +
+  scale_y_continuous(trans="log2", breaks = trans_breaks("log2", function(x) 2^x),
+                     labels = trans_format("log2", math_format(2^.x))) +
+  labs(x = "", y = bquote(Elapsed ~time ~(log[2](seconds))), fill="Method") +  
+  theme_cowplot(font_size = 16) +
+  theme(axis.line.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        legend.position="none")
+
+p_methods_time_2blocks_inter <- ggplot(dsc_plots_2blocks_methods_time, aes_string(x = "method_fac", y = "time", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+  scale_fill_manual(values = colors[c(9,7,12)], labels = c(expression(italic("mr.mash")), "smt-lasso", "e-net")) +
+  scale_y_continuous(trans="log2", breaks = trans_breaks("log2", function(x) 2^x),
+                     labels = trans_format("log2", math_format(2^.x))) +
+  labs(x = "", y = bquote(Elapsed ~time ~(log[2](seconds))), fill="Method") +
+  theme_cowplot(font_size = 16) +
+  theme(axis.line.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        legend.text.align = 0)
+        
+p_methods_time_2blocks <- p_methods_time_2blocks_inter + theme(legend.position="none")
+
+p_methods_time_3causalresp <- ggplot(dsc_plots_3causalresp_methods_time, aes_string(x = "method_fac", y = "time", fill = "method_fac")) +
+  geom_boxplot(color = "black", outlier.size = 1, width = 0.85) +
+  scale_fill_manual(values = colors[c(9,7,12)], labels = c(expression(italic("mr.mash")), "smt-lasso", "e-net")) +
+  scale_y_continuous(trans="log2", breaks = trans_breaks("log2", function(x) 2^x),
+                     labels = trans_format("log2", math_format(2^.x))) +
+  labs(x = "", y = bquote(Elapsed ~time ~(log[2](seconds))), fill="Method") +
+  theme_cowplot(font_size = 16) +
+  theme(axis.line.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        legend.position="none")
+
+
+#Extract legend
+leg_methods_time <- get_legend(p_methods_time_2blocks_inter)
+
+#Make the multi panel plot 
+p_methods_time_all <- plot_grid(p_methods_time_shared, p_methods_time_indep, p_methods_time_2blocks,
+				p_methods_time_1causalresp, p_methods_time_3causalresp, 
+		  		leg_methods_time, labels = c('A', 'B', 'C', 'D', 'E'))
+
+ggsave("../../analysis/paper_figures/mvreg_all_genes_prior_GTExrealX_indepV_missing_Y_mrmash_vs_others_runtime.pdf", plot=p_methods_time_all, device="pdf", units="in", height=8, width=11)
---
+++ code/paper_figures/sims_mvreg_all_genes_prior_GTExrealX_indepV_missing_Y_prep.R
@@ -0,0 +1,120 @@
+###Set options
+options(stringsAsFactors=FALSE)
+
+###Load libraries
+library(dscrutils)
+library(optparse)
+
+###Function to convert dscquery output from list to data.frame suitable for plotting
+convert_dsc_to_dataframe_gtex <- function(dsc){
+  ###Data.frame to store the results after convertion
+  dsc_df <- data.frame()
+  
+  ###Get length of list elements 
+  n_elem <- length(dsc$DSC)
+  
+  ###Loop through the dsc list
+  for(i in 1:n_elem){
+    ##Prepare vectors making up the final data frame
+    r_scalar <- dsc$simulate.r[i]
+    p_causal <- rep(dsc$simulate.p_causal[i], times=r_scalar)
+    r <- rep(dsc$simulate.r[i], times=r_scalar)
+    response <- 1:r_scalar
+    pve <- rep(dsc$simulate.pve[i], times=r_scalar)
+    simulate <- rep(dsc$simulate[i], times=r_scalar)
+    fit <- rep(dsc$fit[i], times=r_scalar)
+    score <- rep(dsc$score[i], times=r_scalar)
+    score.err <- dsc$score.err[[i]]
+    timing <- rep(dsc$fit.time[i], times=r_scalar)
+    
+    ##Build the data frame
+    df <- data.frame(p_num_caus=p_causal, r=r, response=response, pve=pve,  
+                     scenario=simulate, method=fit, score_metric=score, score_value=score.err, time=timing)
+    dsc_df <- rbind(dsc_df, df)
+  }
+  
+  ###Get number of genes
+  n_methods <- length(unique(dsc$fit))
+  n_metrics <- length(unique(dsc$score))
+  n_genes <- n_elem/n_methods/n_metrics
+  
+  ###Compute replicates
+  # beginning <- (repli*n_genes)-(n_genes-1)
+  # ending <- repli*n_genes
+  beginning <- 1
+  ending <- n_genes
+  reptot <- rep(rep(beginning:ending, each=dsc$simulate.r[1]), n_methods*n_metrics)
+  
+  dsc_df <- data.frame(subrep=reptot, dsc_df)
+  
+  return(dsc_df)
+}
+
+###Function to compute rmse (relative to a baseline)
+compute_rrmse <- function(dsc_plot, baseline){
+  dsc_plot <- transform(dsc_plot, experiment=paste(rep, subrep, response, scenario, sep="-"))
+  t <- 0
+  for (i in unique(dsc_plot$experiment)) {
+    t <- t+1
+    rmse_data  <- dsc_plot[which(dsc_plot$experiment == i & dsc_plot$score_metric=="scaled_rmse"), ]
+    mse_mr_mash_consec_em <- rmse_data[which(rmse_data$method==baseline), "score_value"]
+    rmse_data$score_value <- rmse_data$score_value/mse_mr_mash_consec_em
+    rmse_data$score_metric <- "scaled_rrmse"
+    if(t>1){
+      rmse_data_tot <- rbind(rmse_data_tot, rmse_data)
+    } else if(t==1){
+      rmse_data_tot <- rmse_data
+    }
+  }
+  
+  rmse_data_tot$experiment <- NULL
+  
+  return(rmse_data_tot)
+}
+
+###Parse arguments
+parser <- OptionParser()
+parser <- add_option(parser, c("--input"), type="character")
+outparse <- parse_args(parser)
+input <- outparse$input
+
+###Loop over replicates
+for(repl in 1:20){
+  ###Load the dsc results
+  dsc_out <- dscquery(paste0("../../output/", input, "/rep", repl), 
+                       c("simulate.p_causal", "simulate.r", "simulate.r_causal", "simulate.pve", "simulate.B_cor", "simulate.w", 
+                        "simulate.B_scale", "simulate.V_cor", "simulate", "fit", "score", "score.err", "fit.time"), 
+                      groups="fit: mr_mash_em_data_enet, mr_mash_em_can_enet, mr_mash_em_can_mlasso, mr_mash_em_data_mlasso, mr_mash_em_dataAndcan_mlasso, mr_mash_em_dataAndcan_dropcomp_mlasso, mtlasso, enet",
+		      verbose=FALSE)
+ 
+  ###Convert from list to data.frame for plotting
+  if(repl==1){
+    dsc_plots <- convert_dsc_to_dataframe_gtex(dsc_out)
+    dsc_plots <- cbind(rep=repl, dsc_plots)
+  } else if(repl>1){
+    tmp <- convert_dsc_to_dataframe_gtex(dsc_out)
+    tmp <- cbind(rep=repl, tmp)
+    dsc_plots <- rbind(dsc_plots, tmp)
+  }
+}
+
+###Compute rmse score and add it to the data
+rrmse_dat <- compute_rrmse(dsc_plots, baseline="mr_mash_em_data_enet")
+dsc_plots <- rbind(dsc_plots, rrmse_dat)
+
+###Remove mse from scores and keep only methods wanted
+#dsc_plots <- dsc_plots[which(dsc_plots$score_metric!="scaled_rmse" ), ]
+
+###Create factor version of method
+dsc_plots$method_fac <- factor(dsc_plots$method, levels=c("mr_mash_em_can_mlasso", "mr_mash_em_data_mlasso",
+                                                          "mr_mash_em_dataAndcan_mlasso", "mr_mash_em_dataAndcan_dropcomp_mlasso", 
+                                                          "mr_mash_em_can_enet", "mr_mash_em_data_enet", "mtlasso", "enet"),
+                               labels=c("mr_mash_can_mlasso", "mr_mash_data_mlasso", "mr_mash_both_mlasso", 
+                                        "mr_mash_both_drop_mlasso", "mr_mash_can_enet", "mr_mash_data_enet", 
+                                        "mtlasso", "enet"))
+
+###Create factor version of response
+dsc_plots$response_fac <- as.factor(dsc_plots$response)
+
+saveRDS(dsc_plots, paste0("../../output/sims_paper_figures_inter/", input, "_missing_Y_for_plotting.rds"))
+
