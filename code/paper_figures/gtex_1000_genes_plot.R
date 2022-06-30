fc5109dc94468ef42edf0a42b1f6149e1578becb
Fabio Morgante <fabiom@clemson.edu>
2022-06-30 12:55:56 -0500

    Add plot of improvement vs sample size

--- code/paper_figures/gtex_1000_genes_plot.R
+++ code/paper_figures/gtex_1000_genes_plot.R
@@ -10,6 +10,7 @@ library(cowplot)
 dir <- "../../output/gtex_mr_mash_analysis/prediction_score"
 suffix <- "GTEx_V8_score"
 manifest <- read.table("../../data/gtex-v8-manifest-2ormore-tissues-nopath-nosuffix-batch1.txt", header=FALSE)
+sample_size <- read.csv("../../data/gtex-v8-sample-size-by-tissue.csv")
 
 ###Create data frames to store the results
 rmse_enet <- data.frame()
@@ -38,13 +39,14 @@ for(i in 1:nrow(manifest)){
 ###Compute relative improvement of mr.mash over the enet based on RMSE
 rmse_diff <- (as.matrix(rmse_mrmash) - as.matrix(rmse_enet))/as.matrix(rmse_enet)
 medians <- colMedians(rmse_diff, na.rm=TRUE)
+means <- data.frame(tissue=colnames(rmse_diff), mean_RMSE_diff=colMeans2(rmse_diff, na.rm=TRUE))
 rmse_diff <- rmse_diff[, order(medians, decreasing = FALSE)]
 rmse_diff_melt <- reshape2::melt(rmse_diff, value.name="RMSE_diff")
 rmse_diff_melt <- rmse_diff_melt[, -1]
 colnames(rmse_diff_melt)[1] <- "Tissue"
 
 
-###Plot
+###Plot of relative improvement of mr.mash over the enet based on RMSE across tissues
 par(mar = c(13.1,5.4,4,2) + 0.1)
 p <- ggplot(rmse_diff_melt, aes_string(x = "Tissue", y = "RMSE_diff")) +
   geom_boxplot(color = "black", fill="gold", outlier.size = 1, width = 0.85) +
@@ -55,3 +57,20 @@ p <- ggplot(rmse_diff_melt, aes_string(x = "Tissue", y = "RMSE_diff")) +
   theme(axis.text.x = element_text(angle = 45, hjust=1, size=8))
 
 ggsave("../../analysis/paper_figures/gtex_1000_genes_mrmash_vs_enet_rmse.pdf", plot=p, device="pdf", units="in", height=8, width=11)
+
+
+###Compute mean relative improvement of mr.mash over the enet based on RMSE by tissue
+rmse_diff_means <- data.frame(Tissue=colnames(rmse_diff), mean_RMSE_diff=colMeans2(rmse_diff, na.rm=TRUE))
+rmse_diff_means_sample_size <- merge(rmse_diff_means, sample_size, by="Tissue", all.x=TRUE)
+
+###Plot of relative improvement of mr.mash over the enet based on RMSE vs sample size by tissue
+p_size <- ggplot(rmse_diff_means_sample_size, aes(x=X..RNASeq.and.Genotyped.samples, y=mean_RMSE_diff)) + 
+                   geom_point(shape=16) +
+                   geom_smooth(method='lm', formula= y~x) +
+                   labs(x = "Sample size", y = "Difference in RMSE relative to e-net") + 
+                   theme_cowplot(font_size = 16)
+
+ggsave("../../analysis/paper_figures/gtex_1000_genes_mrmash_vs_enet_rmse_vs_sample_size.pdf", plot=p_size, device="pdf", units="in", height=8, width=8)
+
+
+
